ef1d336ad542c2d2e57846d8e649c7a0035bb090
Wilhelm Söderkvist Vermelin <wilhelm.wermelin@icloud.com>
2024-11-15 10:28:27 -0600

    refactor ui code

--- src/ui.rs
+++ src/ui.rs
@@ -46,58 +46,7 @@ fn left_aligned_rect(r: Rect, p: u16) -> Rect {
     Layout::horizontal([Constraint::Fill(1), Constraint::Percentage(100 - p)]).split(r)[0]
 }
 
-pub fn render(f: &mut Frame, app: &mut App) {
-    f.render_widget(
-        Block::bordered()
-            .title("AI in the Terminal")
-            .title_alignment(Alignment::Center)
-            .border_type(BorderType::Rounded),
-        f.area(),
-    );
-    let input_area_constraint = match app.app_mode {
-        AppMode::Editing => Constraint::Min(1),
-        _ => Constraint::Length(0),
-    };
-
-    let vertical = Layout::vertical([
-        Constraint::Length(1),
-        Constraint::Min(1),
-        input_area_constraint,
-    ]);
-
-    let vertical = vertical.margin(1);
-
-    let [help_area, messages_area, input_area] = vertical.areas(f.area());
-
-    let (msg, style) = match app.app_mode {
-        AppMode::Editing => (
-            vec![
-                "Press ".into(),
-                "Esc".bold(),
-                " to stop editing. Press ".into(),
-                "CONTROL + S (C-s)".bold(),
-                " to submit the message.".into(),
-            ],
-            Style::default(),
-        ),
-        _ => (
-            vec![
-                "Press ".into(),
-                "Esc/q".bold(),
-                " to exit. Press ".into(),
-                "?".bold(),
-                " for help.".into(),
-            ],
-            Style::default(),
-        ),
-    };
-    let text = Text::from(Line::from(msg)).patch_style(style);
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, help_area);
-
-    if let AppMode::Editing = app.app_mode {
-        f.render_widget(&app.input_textarea, input_area);
-    }
+fn render_messages(f: &mut Frame, app: &mut App, messages_area: Rect) {
     let messages: Vec<Line> = app
         .messages
         .iter()
@@ -162,148 +111,205 @@ pub fn render(f: &mut Frame, app: &mut App) {
         }),
         &mut scrollbar_state,
     );
-    if let AppMode::ModelSelection = app.app_mode {
-        let block = Block::bordered().title("Select Model");
-        let area = centered_rect(40, 50, messages_area);
-        f.render_widget(Clear, area); //this clears out the background
-        f.render_widget(block, area);
-        render_model_list(f, area, app);
-    }
+}
 
-    if let AppMode::SnippetSelection = app.app_mode {
-        let block = Block::bordered().title("Select Snippet");
-        let area = centered_rect(20, 100, messages_area);
-        f.render_widget(Clear, area); //this clears out the background
-        f.render_widget(block, area);
-        render_snippet_list(f, area, app);
+pub fn render(f: &mut Frame, app: &mut App) {
+    f.render_widget(
+        Block::bordered()
+            .title("AI in the Terminal")
+            .title_alignment(Alignment::Center)
+            .border_type(BorderType::Rounded),
+        f.area(),
+    );
 
-        let preview_block = Block::bordered().title("Snippet Preview");
-        let preview_area = right_aligned_rect(messages_area, 40);
-        f.render_widget(Clear, preview_area); //this clears out the background
-        f.render_widget(preview_block, preview_area);
-        let preview_text = app.get_snippet_text();
-        let preview_block_content = Block::new().padding(Padding::uniform(1));
-        if let Some(preview_text) = preview_text {
-            let snippet_paragraph = Paragraph::new(Text::from(preview_text.as_str()).magenta())
-                .block(preview_block_content);
-            f.render_widget(snippet_paragraph, preview_area);
+    let input_area_constraint = match app.app_mode {
+        AppMode::Editing => Constraint::Min(1),
+        _ => Constraint::Length(0),
+    };
+
+    let vertical = Layout::vertical([
+        Constraint::Length(1),
+        Constraint::Min(1),
+        input_area_constraint,
+    ]);
+
+    let vertical = vertical.margin(1);
+
+    let [help_area, messages_area, input_area] = vertical.areas(f.area());
+
+    match app.app_mode {
+        AppMode::Normal => {
+            render_messages(f, app, messages_area);
+        }
+        AppMode::Editing => {
+            render_messages(f, app, messages_area);
+            f.render_widget(&app.input_textarea, input_area);
+        }
+        AppMode::ModelSelection => {
+            let block = Block::bordered().title("Select Model");
+            let area = centered_rect(40, 50, messages_area);
+            f.render_widget(Clear, area); //this clears out the background
+            f.render_widget(block, area);
+            render_model_list(f, area, app);
+        }
+        AppMode::SnippetSelection => {
+            let block = Block::bordered().title("Select Snippet");
+            let area = centered_rect(20, 100, messages_area);
+            f.render_widget(Clear, area); //this clears out the background
+            f.render_widget(block, area);
+            render_snippet_list(f, area, app);
+
+            let preview_block = Block::bordered().title("Snippet Preview");
+            let preview_area = right_aligned_rect(messages_area, 40);
+            f.render_widget(Clear, preview_area); //this clears out the background
+            f.render_widget(preview_block, preview_area);
+            let preview_text = app.get_snippet_text();
+            let preview_block_content = Block::new().padding(Padding::uniform(1));
+            if let Some(preview_text) = preview_text {
+                let snippet_paragraph = Paragraph::new(Text::from(preview_text.as_str()).magenta())
+                    .block(preview_block_content);
+                f.render_widget(snippet_paragraph, preview_area);
+            }
+        }
+        AppMode::ShowHistory => {
+            let block = Block::bordered().title("Select Chat");
+            let area = left_aligned_rect(messages_area, 25);
+            f.render_widget(Clear, area); //this clears out the background
+            f.render_widget(block, area);
+            render_chat_history_list(f, area, app);
+
+            let preview_block = Block::bordered().title("Chat Preview");
+            let preview_area = right_aligned_rect(messages_area, 75);
+            f.render_widget(Clear, preview_area); //this clears out the background
+            f.render_widget(preview_block, preview_area);
+            let chat_id = app.get_selected_chat_id();
+            let preview_text = if let Some(id) = chat_id {
+                let text = list_all_messages(*id)
+                    .unwrap_or([].to_vec())
+                    .into_iter()
+                    .map(|m| match m {
+                        Message::User(t) => format!("USER: {}\n", t),
+                        Message::Assistant(t) => format!("ASSISTANT: {}\n", t),
+                        Message::Error(t) => format!("ERROR: {}\n", t),
+                    })
+                    .collect::<Vec<String>>()
+                    .join("\n");
+                Some(text)
+            } else {
+                None
+            };
+            let preview_block_content = Block::new().padding(Padding::uniform(1));
+            if let Some(preview_text) = preview_text {
+                let snippet_paragraph = Paragraph::new(Text::from(preview_text.as_str()).magenta())
+                    .wrap(Wrap { trim: true })
+                    .block(preview_block_content);
+                f.render_widget(snippet_paragraph, preview_area);
+            }
+        }
+        AppMode::Help => {
+            let block = Block::bordered().title("Help");
+            let area = centered_rect(50, 60, messages_area);
+            f.render_widget(Clear, area); //this clears out the background
+            f.render_widget(block, area);
+
+            let normal_keys = vec![
+                "Press ".into(),
+                "Esc/q".bold(),
+                " to exit, ".into(),
+                "i".bold(),
+                " to start editing, ".into(),
+                "y".bold(),
+                " to copy the last answer (not linux yet), ".into(),
+                "m".bold(),
+                " to choose model, ".into(),
+                "h".bold(),
+                " to browse previous conversations, ".into(),
+                "s".bold(),
+                " to browse code snippets.".into(),
+            ];
+            let editing_keys = vec![
+                "Press ".into(),
+                "Esc".bold(),
+                " to stop editing. Press ".into(),
+                "CONTROL + S (C-s)".bold(),
+                " to submit the message. ".into(),
+                "Paste into the text area by pressing ".into(),
+                "Ctrl + V".bold(),
+            ];
+            let model_keys = vec![
+                "Press ".into(),
+                "Up/Down".bold(),
+                " to select model, or press ".into(),
+                "Enter".bold(),
+                " to select model, and return to 'normal' mode.".into(),
+            ];
+            let chat_keys = vec![
+                "Press ".into(),
+                "Up/Down".bold(),
+                " to select chat, or press ".into(),
+                "d".bold(),
+                " to delete the selected chat, or press ".into(),
+                "Enter".bold(),
+                " to select a chat, and return to 'normal' mode.".into(),
+            ];
+            let snippet_keys = vec![
+                "Press ".into(),
+                "Up/Down".bold(),
+                " to select snippet, or press ".into(),
+                "Enter".bold(),
+                " to copy snippet to the clipboard (not linux yet), and return to 'normal' mode."
+                    .into(),
+            ];
+            let msg = vec![
+                Line::from(Span::raw("Welcome to AI in the Terminal! ").bold()),
+                Line::from(""),
+                Line::from(Span::raw("When in 'normal' mode, you can:").bold()),
+                Line::from(normal_keys),
+                Line::from(""),
+                Line::from(Span::raw("When in 'editing' mode, you can:").bold()),
+                Line::from(editing_keys),
+                Line::from(""),
+                Line::from(Span::raw("When choosing models, you can:").bold()),
+                Line::from(model_keys),
+                Line::from(""),
+                Line::from(Span::raw("When choosing chats, you can:").bold()),
+                Line::from(chat_keys),
+                Line::from(""),
+                Line::from(Span::raw("When browsing snippets, you can:").bold()),
+                Line::from(snippet_keys),
+            ];
+            let help_text_block = Block::new().padding(Padding::uniform(1));
+            let text = Text::from(msg).patch_style(Style::default());
+            let help_message = Paragraph::new(text)
+                .block(help_text_block)
+                .wrap(Wrap { trim: true });
+            f.render_widget(help_message, area);
         }
     }
 
-    if let AppMode::ShowHistory = app.app_mode {
-        let block = Block::bordered().title("Select Chat");
-        let area = left_aligned_rect(messages_area, 25);
-        f.render_widget(Clear, area); //this clears out the background
-        f.render_widget(block, area);
-        render_chat_history_list(f, area, app);
-
-        let preview_block = Block::bordered().title("Chat Preview");
-        let preview_area = right_aligned_rect(messages_area, 75);
-        f.render_widget(Clear, preview_area); //this clears out the background
-        f.render_widget(preview_block, preview_area);
-        let chat_id = app.get_selected_chat_id();
-        let preview_text = if let Some(id) = chat_id {
-            let text = list_all_messages(*id)
-                .unwrap_or([].to_vec())
-                .into_iter()
-                .map(|m| match m {
-                    Message::User(t) => format!("USER: {}\n", t),
-                    Message::Assistant(t) => format!("ASSISTANT: {}\n", t),
-                    Message::Error(t) => format!("ERROR: {}\n", t),
-                })
-                .collect::<Vec<String>>()
-                .join("\n");
-            Some(text)
-        } else {
-            None
-        };
-        let preview_block_content = Block::new().padding(Padding::uniform(1));
-        if let Some(preview_text) = preview_text {
-            let snippet_paragraph = Paragraph::new(Text::from(preview_text.as_str()).magenta())
-                .wrap(Wrap { trim: true })
-                .block(preview_block_content);
-            f.render_widget(snippet_paragraph, preview_area);
+    let msg = match app.app_mode {
+        AppMode::Editing => {
+            vec![
+                "Press ".into(),
+                "Esc".bold(),
+                " to stop editing. Press ".into(),
+                "CONTROL + S (C-s)".bold(),
+                " to submit the message.".into(),
+            ]
         }
-    }
-    if let AppMode::Help = app.app_mode {
-        let block = Block::bordered().title("Help");
-        let area = centered_rect(50, 60, messages_area);
-        f.render_widget(Clear, area); //this clears out the background
-        f.render_widget(block, area);
-
-        let normal_keys = vec![
-            "Press ".into(),
-            "Esc/q".bold(),
-            " to exit, ".into(),
-            "i".bold(),
-            " to start editing, ".into(),
-            "y".bold(),
-            " to copy the last answer (not linux yet), ".into(),
-            "m".bold(),
-            " to choose model, ".into(),
-            "h".bold(),
-            " to browse previous conversations, ".into(),
-            "s".bold(),
-            " to browse code snippets.".into(),
-        ];
-        let editing_keys = vec![
-            "Press ".into(),
-            "Esc".bold(),
-            " to stop editing. Press ".into(),
-            "CONTROL + S (C-s)".bold(),
-            " to submit the message. ".into(),
-            "Paste into the text area by pressing ".into(),
-            "Ctrl + V".bold(),
-        ];
-        let model_keys = vec![
-            "Press ".into(),
-            "Up/Down".bold(),
-            " to select model, or press ".into(),
-            "Enter".bold(),
-            " to select model, and return to 'normal' mode.".into(),
-        ];
-        let chat_keys = vec![
-            "Press ".into(),
-            "Up/Down".bold(),
-            " to select chat, or press ".into(),
-            "d".bold(),
-            " to delete the selected chat, or press ".into(),
-            "Enter".bold(),
-            " to select a chat, and return to 'normal' mode.".into(),
-        ];
-        let snippet_keys = vec![
-            "Press ".into(),
-            "Up/Down".bold(),
-            " to select snippet, or press ".into(),
-            "Enter".bold(),
-            " to copy snippet to the clipboard (not linux yet), and return to 'normal' mode."
-                .into(),
-        ];
-        let msg = vec![
-            Line::from(Span::raw("Welcome to AI in the Terminal! ").bold()),
-            Line::from(""),
-            Line::from(Span::raw("When in 'normal' mode, you can:").bold()),
-            Line::from(normal_keys),
-            Line::from(""),
-            Line::from(Span::raw("When in 'editing' mode, you can:").bold()),
-            Line::from(editing_keys),
-            Line::from(""),
-            Line::from(Span::raw("When choosing models, you can:").bold()),
-            Line::from(model_keys),
-            Line::from(""),
-            Line::from(Span::raw("When choosing chats, you can:").bold()),
-            Line::from(chat_keys),
-            Line::from(""),
-            Line::from(Span::raw("When browsing snippets, you can:").bold()),
-            Line::from(snippet_keys),
-        ];
-        let help_text_block = Block::new().padding(Padding::uniform(1));
-        let text = Text::from(msg).patch_style(style);
-        let help_message = Paragraph::new(text)
-            .block(help_text_block)
-            .wrap(Wrap { trim: true });
-        f.render_widget(help_message, area);
-    }
+        _ => {
+            vec![
+                "Press ".into(),
+                "Esc/q".bold(),
+                " to exit. Press ".into(),
+                "?".bold(),
+                " for help.".into(),
+            ]
+        }
+    };
+    let text = Text::from(Line::from(msg)).patch_style(Style::default());
+    let help_message = Paragraph::new(text);
+    f.render_widget(help_message, help_area);
 
     #[cfg(not(target_os = "linux"))]
     {
